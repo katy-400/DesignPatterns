4314562be9b882e91f0d9ca1255861e9811b6425
katy-400 <katy.nyby@gmail.com>
2016-12-20 19:52:46 -0500

    Add files via upload

--- program4.php
+++ program4.php
@@ -30,3 +30,39 @@ class dbconn
     return $this->conn;
   }
 }
+//Factory
+class Customer
+{
+  private $custName = "";
+  private $contFirst = "";
+  private $contLast = "";
+  private $addressLine1 = "";
+  private $Phone = 0;
+  function __construct() {}
+  function setName($name) {$this->custName = $name; }
+  function setFirst($first) {$this->custFirst = $first; }
+  function setLast($last) {$this->custLast = $last; }
+  function setAdress($address) {$this->addressLine1 = $address; }
+  function setPhone ($phone) {$this->phone = $phone;}
+  function profile()
+  {
+    echo "<h3>Business: $this->custName</h3>
+         <b>Person to Contact: </b>  $this->custFirst $this->custLast <br>
+         <b>Address:  </b>  $this->addressLine1 <br>
+         <b>Phone: </b> $this->phone <br><br><br><br>";
+  }
+}
+class CustomerFactory //creates customers given the content
+{
+  public static function makeCustomer($customerName, $contactFName, $contactLName, $addressLine1, $phone)
+  {
+    $cust = new Customer();
+    $cust->setName($customerName);
+    $cust->setFirst($contactFName);
+    $cust->setLast($contactLName);
+    $cust->setAdress($addressLine1);
+    $cust->setPhone($phone);
+   
+    return $cust;
+  }
+}

8619acecdc77b03b439a20d7088a57fe214562c9
katy-400 <katy.nyby@gmail.com>
2016-12-20 19:51:31 -0500

    Add files via upload

---
+++ program5.php
@@ -0,0 +1,32 @@
+<?php
+//Singleton
+class dbconn 
+{
+  
+  private static $instance = null;
+  private $conn;
+  
+  private $host = 'sql1.njit.edu';
+  private $user = 'kn96';
+  private $pass = 'm6m5PRHab';
+  private $db = 'kn96';
+  
+  
+  private function __construct()
+  {
+    $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+    if ($this->conn->connect_error){
+      die("NOT CONNECTED" . $this->conn->connect_error);
+      }
+  }
+  public static function getInstance(){
+    if(!self::$instance){
+	      self::$instance = new dbconn();
+      }
+   return self::$instance;
+    }
+  
+  public function getConnection() {
+    return $this->conn;
+  }
+}
